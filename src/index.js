c735fccb158095f87e4c5abe8efe08459896ae2a
eliorsastiel1 <32665612+eliorsastiel1@users.noreply.github.com>
2018-11-03 10:00:30 +0200

    book-app

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { HashRouter } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+	<HashRouter>
+		<App />
+	</HashRouter>,
+	// <BrowserRouter>
+	// 	<App />
+	// </BrowserRouter>,
+	document.getElementById('root')
+);
+
